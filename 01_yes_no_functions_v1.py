caac55cb2d5dd6f5a87f6bb9a431efad39d8baae
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-03-24 18:55:43 +1300

    Create 01_yes_no_functions_v1.py

---
+++ 01_yes_no_functions_v1.py
@@ -0,0 +1,19 @@
+show_instructions = ""
+while show_instructions != "x":
+    # Ask the user if they have played before
+    show_instructions = input('Have you played this game before? (Yes/No): ').lower()
+
+    # If they say yes, output "Program Continues"
+    if show_instructions == 'yes' or show_instructions == "y":
+        print("-Program continues-")
+        break
+
+    # If they say no, output "Display Instructions"
+    elif show_instructions == 'no' or show_instructions == "n":
+        print("-Instructions for program-")
+
+    # Otherwise - show error
+    else:
+        print("Error: Please put either Yes or No")
+
+    print(f"You entered '{show_instructions}'")
