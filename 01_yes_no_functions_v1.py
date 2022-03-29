3ee91262b614af6b754a22c18f992ba1710dfe44
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-03-29 18:29:24 +1300

    Update 01_yes_no_functions_v1.py

--- 01_yes_no_functions_v1.py
+++ 01_yes_no_functions_v1.py
@@ -1,19 +1,29 @@
-show_instructions = ""
-while show_instructions != "x":
-    # Ask the user if they have played before
-    show_instructions = input('Have you played this game before? (Yes/No): ').lower()
 
-    # If they say yes, output "Program Continues"
-    if show_instructions == 'yes' or show_instructions == "y":
-        print("-Program continues-")
-        break
+# Functions
+def yes_no(question_text):
+    while True:
 
-    # If they say no, output "Display Instructions"
-    elif show_instructions == 'no' or show_instructions == "n":
-        print("-Instructions for program-")
 
-    # Otherwise - show error
-    else:
-        print("Error: Please put either Yes or No")
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
 
-    print(f"You entered '{show_instructions}'")
+        # If they say yes, output "Program Continues"
+        if answer == 'yes' or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If they say no, output "Display Instructions"
+        elif answer == 'no' or answer == "n":
+            answer = "No"
+            return answer
+
+        # Otherwise - show error
+        else:
+            print("Error: Please put either Yes or No")
+
+# main routine
+show_instructions = yes_no("Have you played this game before? (Yes/No): ")
+print(f"You entered '{show_instructions}'")
+print()
+having_fun = yes_no
+print(f"You entered '{having_fun}'")
