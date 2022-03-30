a2f1f3027a90fee5b7ba2898b32b37cdcfbcc2d9
Jaxku <52820208+Jaxku@users.noreply.github.com>
2022-03-30 14:57:24 +1300

    v2 done

--- 03_how_much_v2.py
+++ 03_how_much_v2.py
@@ -1,15 +1,21 @@
-"""Component 2 (How much) v1
-Ask user how much they wont to play with and check that the input is a valid
-integer between 1 and 10. If it is, this amount becomes the balance in their
-account"""
+"""Component 2 (How much) v2
+Use try/accept and pull error message out of the loop"""
 
-user_balance = int(input("How much do you want to play with (must be an ""integer between 1 amd 10) $"))
+error = "Please enter a whole number between 1 and 10\n"
+valid = False
 
-# keep asking until a valid input is entered (1-10)
-while not 1 <= user_balance <= 10:
-    print("Try again. lease enter aw whole number between 1 and 10")
-    # ask for the input
-    user_balance = int(input("how much do you want to play with $"))
+# Keep asking until a valid amount (1-10) is entered
+while not valid:
+    try:
+            # ask for amount
+    user_balance = int(input("How much would you like to play with? $"))
 
-print(f"You are playing with ${user_balance}")
+    # check if the amount is too high/low
+    if 0 < user_balance <= 10:
+        print(f"You are playing with ${user_balance}")
+        valid = True
+    else:
+        print(error)
 
+    expect ValueError:
+        print(error)
